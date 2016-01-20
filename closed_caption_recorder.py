dd795efb1c3c65fb833705cd6c82519b4fe8b5d0
Jonathan <poissonj@ufl.edu>
2016-01-20 14:50:45 -0500

    Added in initial version of the script

---
+++ closed_caption_recorder.py
@@ -0,0 +1,75 @@
+import time
+from contextlib import closing
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver import Firefox # pip install selenium
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+
+with open("urls.txt", "r") as myfile:
+    list_of_urls = myfile.readlines()
+
+for url in list_of_urls:
+    print url
+
+    # Get website
+    browser = webdriver.Firefox()
+    browser.get(url)
+
+    page_title = browser.title
+    print page_title
+
+    # Give time for javascript to being playing video
+    time.sleep(3.00)
+
+    video_playing = True
+
+    # Click cc button
+    if browser.find_element_by_css_selector("button.cc"):
+        cc_button = browser.find_element_by_css_selector("button.cc")
+        cc_button.click()
+
+    # Click rate button to make the video play faster
+    if browser.find_element_by_css_selector("button.rate"):
+        cc_button = browser.find_element_by_css_selector("button.rate")
+
+        # Click 3 times to go to fastest speed
+        cc_button.click()
+        cc_button.click()
+        cc_button.click()
+
+    # Grab video duration to know when video ends
+    video_duration = browser.find_element_by_class_name("duration").text
+    print "Video Duration: " + video_duration
+
+    already_saved_subtitle_text = ""
+    closed_captions = []
+
+    while video_playing:
+        current_video_time = browser.find_element_by_class_name("position").text
+        if current_video_time == video_duration:
+            break
+
+        try:
+            try:
+                current_subtitle_text = browser.find_element_by_class_name("ui-playercaptionpanel-item").text
+            except NoSuchElementException:
+                if current_video_time == video_duration:
+                    break
+                continue
+            if (current_subtitle_text != "" or current_subtitle_text != null) and current_subtitle_text != already_saved_subtitle_text:
+                print current_subtitle_text
+                closed_captions.append(current_subtitle_text)
+                already_saved_subtitle_text = current_subtitle_text
+        except StaleElementReferenceException:
+            continue
+
+    # Write output
+    # print ' '.join(closed_captions)
+    with open("closed_captions.txt", "a") as text_file:
+        text_file.write("\n\n" + page_title)
+        text_file.write(' '.join(closed_captions))
+        text_file.close()
+
+    browser.close()
